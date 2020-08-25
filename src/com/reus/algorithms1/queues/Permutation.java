7fc826c1924d19a8fabd52fb542aae682dfbd9b2
Konstantin Reus <konstantin.reus@zalando.de>
2020-08-25 08:02:16 +0200

    [Week2] add bonus - select K random from infinite stream

--- src/com/reus/algorithms1/queues/Permutation.java
+++ src/com/reus/algorithms1/queues/Permutation.java
@@ -1,6 +1,7 @@
 package com.reus.algorithms1.queues;
 
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
@@ -9,8 +10,22 @@ public class Permutation {
         }
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<>();
+        int current = 1;
         while (!StdIn.isEmpty()) {
-            queue.enqueue(StdIn.readString());
+            String input = StdIn.readString();
+            if (current <= k) {
+                queue.enqueue(input);
+            } else {
+                double chance = k * 1.0 / current;
+                double random = StdRandom.uniform();
+                if (random < chance) {
+                    if (!queue.isEmpty()) {
+                        queue.dequeue();
+                    }
+                    queue.enqueue(input);
+                }
+            }
+            current++;
         }
         for (int i = 0; i < k; i++) {
             System.out.println(queue.dequeue());
