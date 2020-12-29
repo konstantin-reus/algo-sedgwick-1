8e796ef001b579519080120a0eb5e83071628730
Konstantin Reus <konstantin.reus@zalando.de>
2020-12-29 23:07:35 +0300

    [Week4] Boggle 100/100

--- src/com/reus/algorithms2/boggle/TrieSet.java
+++ src/com/reus/algorithms2/boggle/TrieSet.java
@@ -4,29 +4,8 @@ import edu.princeton.cs.algs4.Queue;
 
 import java.util.Iterator;
 
-/**
- * The {@code TrieSET} class represents an ordered set of strings over
- * the extended ASCII alphabet.
- * It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
- * methods. It also provides character-based methods for finding the string
- * in the set that is the <em>longest prefix</em> of a given prefix,
- * finding all strings in the set that <em>start with</em> a given prefix,
- * and finding all strings in the set that <em>match</em> a given pattern.
- * <p>
- * This implementation uses a 256-way trie.
- * The <em>add</em>, <em>contains</em>, <em>delete</em>, and
- * <em>longest prefix</em> methods take time proportional to the length
- * of the key (in the worst case). Construction takes constant time.
- * <p>
- * For additional documentation, see
- * <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- * <i>Algorithms in Java, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- * @author Robert Sedgewick
- * @author Kevin Wayne
- */
 public class TrieSet implements Iterable<String> {
-    private static final int R = 256;        // extended ASCII
+    private static final int R = 26;
 
     private Node root;      // root of trie
     private int n;          // number of keys in trie
@@ -56,14 +35,14 @@ public class TrieSet implements Iterable<String> {
         if (x == null) return null;
         if (d == key.length()) return x;
         char c = key.charAt(d);
-        return get(x.next[c], key, d + 1);
+        return get(x.next[c - 'A'], key, d + 1);
     }
 
     private Node get(Node x, char c) {
         if (x == null) {
             return null;
         }
-        return x.next[c];
+        return x.next[c - 'A'];
     }
 
     /**
@@ -85,7 +64,7 @@ public class TrieSet implements Iterable<String> {
         } else {
             char c = key.charAt(d);
             x.hasChildren = true;
-            x.next[c] = add(x.next[c], key, d + 1);
+            x.next[c - 'A'] = add(x.next[c - 'A'], key, d + 1);
         }
         return x;
     }
@@ -159,7 +138,7 @@ public class TrieSet implements Iterable<String> {
         if (x.isString) results.enqueue(prefix.toString());
         for (char c = 0; c < R; c++) {
             prefix.append(c);
-            collect(x.next[c], prefix, results);
+            collect(x.next[c - 'A'], prefix, results);
             prefix.deleteCharAt(prefix.length() - 1);
         }
     }
